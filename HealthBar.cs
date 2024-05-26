2e7754cd197c39c718ce11a7dbfccc2f0ca36bc4
Vladislav Golosov <v.golosoff@gmail.com>
2024-05-27 01:21:49 +0300

    Final

--- HealthBar.cs
+++ HealthBar.cs
@@ -7,11 +7,47 @@ namespace HealthBar
 
         private static void Main(string[] args)
         {
-            int health = 50, maxHealth = 100;
-            int mana = 90, maxMana = 100;
+            int health = 5, maxHealth = 10;
+            int mana = 9, maxMana = 10;
 
+            Console.Clear();
             DrawBar(health, maxHealth, ConsoleColor.Red,0);
             DrawBar(mana, maxMana, ConsoleColor.Blue,1);
+
+            Console.SetCursorPosition(0, 5);
+            Console.WriteLine("Введите количество здоровья(0-10): ");
+            int tempHealth = Convert.ToInt32(Console.ReadLine());
+            if (tempHealth >= 0 && tempHealth <= 10)
+            {
+                health = tempHealth;
+            }
+            else
+            {
+                Console.WriteLine("Неверное значение");
+            }
+            Console.WriteLine("Введите количество маны(0-10): ");
+            int tempMana = Convert.ToInt32(Console.ReadLine());
+            if (tempMana >= 0 && tempMana <= 10)
+            {
+                mana = tempMana;
+                Console.Clear();
+                DrawBar(health, maxHealth, ConsoleColor.Red,0);
+                DrawBar(mana, maxMana, ConsoleColor.Blue,1);
+            }
+            else
+            {
+                Console.WriteLine("Неверное значение");
+            }
+
+
+            Console.ReadLine();
+            Console.Clear();
+
+
+                
+
+            
+            
         }
 
         static void DrawBar(int value, int maxValue, ConsoleColor color, int position)
